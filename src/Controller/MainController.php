ab56e507e94fccb2db33c6e187f23cacca39659d
momo22 <mohamed.boulligh@gmail.com>
2023-01-03 11:05:55 +0100

    mise en place squelette + page accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,7 @@ class MainController extends AbstractController
     /**
      * Controleur de la page d'accueil
      */
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'main_home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
