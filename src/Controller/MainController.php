11655e97fafca53695c02faaa6306244c86ef554
momo22 <mohamed.boulligh@gmail.com>
2023-01-05 13:11:21 +0100

    ajout page de profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,18 @@ class MainController extends AbstractController
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * controleur de la page de profil
+     *
+     * aces reservé au connecter (role_user)
+     */
+    #[Route('/mon-profil/', name: 'main_profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+
+        return $this->render('main/profil.html.twig');
     }
 }
