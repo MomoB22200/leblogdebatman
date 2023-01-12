00b7a343e0cedad1e4d4e459de2214f39b8f366a
momo22 <mohamed.boulligh@gmail.com>
2023-01-12 12:24:10 +0100

    ajout upload photo de profil

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\EditPhotoType;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,4 +34,46 @@ class MainController extends AbstractController
 
         return $this->render('main/profil.html.twig');
     }
+
+    /**
+     * Controleur de la page page de modification de la photo de profil
+     */
+    #[Route('/changer-photo-de-profil/', name: 'main_edit_photo')]
+    #[IsGranted('ROLE_USER')]
+    public function editPhoto(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $form = $this->createForm(EditPhotoType::class);
+
+        $form->handleRequest($request);
+
+        //si le formulaire a été envoyer et sans erreurs
+        if($form->isSubmitted() && $form->isValid()){
+
+            $photo = $form->get('photo')->getData();
+
+            $newFileName = 'user' . $this->getUser()->getId() . '.' . $photo->guessExtension();
+
+            //mise a jour du nom de la photo du compte connecté
+            $this->getUser()->setPhoto($newFileName);
+            $em = $doctrine->getManager();
+            $em->flush();
+
+            //sauvegarde physique de la photo coté serveur
+            $photo->move(
+                $this->getParameter('app.user.photo.directory'),
+                $newFileName
+            );
+
+            //message de succès
+            $this->addFlash('success', 'Photo de profil modifiée avec succès');
+
+            //redirection vers la page de profil
+            return $this->redirectToRoute('main_profil');
+        }
+
+        return $this->render('main/edit_photo.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
